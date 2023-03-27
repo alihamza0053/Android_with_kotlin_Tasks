b72107993eeb414d6e8ba8a17dc2395c81e72f8a
Ali Hamza <alihamza00053@gmail.com>
2023-03-27 14:27:59 +0500

    change

---
+++ readStringWithKeys/src/main/kotlin/readString.kt
@@ -0,0 +1,18 @@
+import java.security.Key
+import java.util.Dictionary
+
+fun main(){
+    var string = "api.bytewise.com/{path}/{student}/kotlin/{name}/{rollnumber}"
+    println("running")
+    var a = string.repleaseKeys("path" to "android","student" to "CS","name" to "hamza", "rollnumber" to "50")
+    println(a)
+
+}
+
+fun String.repleaseKeys(vararg values: Pair<Any,String>): String{
+    var result = this
+    for ((key,value) in values){
+        result = result.replace("{$key}",value.toString())
+    }
+    return result
+}
